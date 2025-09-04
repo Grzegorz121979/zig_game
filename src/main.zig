b0308cbe9e30b3d939ff4f7d15e6aa8bcc2d1cd5
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-09-04 17:36:10 +0200

    Adding function to reading from consol

--- src/main.zig
+++ src/main.zig
@@ -14,13 +14,16 @@ pub fn main() !void {
     const stdin = &stdin_reader.interface;
     const stdout = &stdout_writer.interface;
 
-    stdout.writeAll("Hello, world!");
-    _ = stdin;
+    try stdout.writeAll("What's your name: ");
+    const name = try stdin.takeDelimiterExclusive('\n');
+    const trimmed_name = std.mem.trim(u8, name, " \n\r\t");
+    try stdout.print("Hello, {s}!", .{trimmed_name});
+    try stdout.flush();
 
-    const user = User {
-        .name = "Greg",
-        .age = 46,
-    };
+    // const user = User {
+    //     .name = "Greg",
+    //     .age = 46,
+    // };
 
-    std.debug.print("Name: {s}, age: {d}\n", .{user.name, user.age});
+    // std.debug.print("Name: {s}, age: {d}\n", .{user.name, user.age});
 }
