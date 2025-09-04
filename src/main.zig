590ff868c511f1d55162d09290550164bd7dfebd
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-09-04 13:52:34 +0200

    adding struct to code

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
+const User = struct {
+    name: []const u8,
+    age: u8,
+};
+
 pub fn main() !void {
-    std.debug.print("Hello, world!", .{});
+    const user = User {
+        .name = "Greg",
+        .age = 46,
+    };
+
+    std.debug.print("Name: {s}, age: {d}\n", .{user.name, user.age});
 }
