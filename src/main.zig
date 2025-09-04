5f5e3d0a739ac4d5b540eeedc19898b1b3fe1ab8
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-09-04 17:08:35 +0200

    Addin stdin ana stdout interface

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,17 @@ const User = struct {
 };
 
 pub fn main() !void {
+
+    var stdin_buffer: [16]u8 = undefined;
+    var stdout_buffer: [16]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdin = &stdin_reader.interface;
+    const stdout = &stdout_writer.interface;
+
+    stdout.writeAll("Hello, world!");
+    _ = stdin;
+
     const user = User {
         .name = "Greg",
         .age = 46,
